ff795b19d47fa53d0f0f8973fef955d4806c360e
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2025-11-22 10:28:12 +0200

    Add files via upload

---
+++ dpkg -i.sh
@@ -0,0 +1,9 @@
+dpkg -i /tmp/*.deb
+wait
+apt-get update && dpkg -i --force-overwrite /tmp/*.deb; apt-get install -f -y
+wait
+opkg install wget
+wait
+apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
+
+
---
+++ dpkg.sh
@@ -0,0 +1,3 @@
+dpkg --configure -a
+wait
+apt-get update && apt-get upgrade
